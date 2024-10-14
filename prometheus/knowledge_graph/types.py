b34e4de251095f2903b778d13b4bfe917fe46530
chenzimin <chen.zimin@hotmail.com>
2024-10-14 22:22:07 +0200

    Add knowledge graph types and test

---
+++ prometheus/knowledge_graph/types.py
@@ -0,0 +1,187 @@
+import dataclasses
+import enum
+from typing import Sequence, TypedDict, Union
+
+
+@dataclasses.dataclass
+class FileNode:
+  """A node representing a file/dir.
+
+  Attributes:
+    basename: The basename of a file/dir, like 'bar.py' or 'foo'.
+    relative_path: The relative path from the root path, like 'foo/bar/baz.java'.
+  """
+
+  basename: str
+  relative_path: str
+
+
+@dataclasses.dataclass
+class ASTNode:
+  """A node representing a tree-sitter node.
+
+  Attributes:
+    type: The tree-sitter node type.
+    start_line: The starting line number.
+    end_line: The ending line number.
+    text: The source code correcpsonding to the node.
+  """
+
+  type: str
+  start_line: int
+  end_line: int
+  text: str
+
+
+@dataclasses.dataclass
+class TextNode:
+  """A node representing a piece of text.
+
+  Attributes:
+    text: A string.
+  """
+
+  text: str
+
+
+@dataclasses.dataclass
+class KnowledgeGraphNode:
+  """A node in the knowledge graph.
+
+  Attributes:
+    node_id: A id that uniquely identifies a node in the graph.
+    node: The node itself, can be a FileNode, ASTNode or TextNode.
+  """
+
+  node_id: int
+  node: Union[FileNode, ASTNode, TextNode]
+
+  def to_neo4j_node(self) -> Union["Neo4jFileNode", "Neo4jASTNode", "Neo4jTextNode"]:
+    """Convert the KnowledgeGraphNode into a Neo4j node format."""
+    match self.node:
+      case FileNode():
+        return Neo4jFileNode(
+          node_id=self.node_id,
+          basename=self.node.basename,
+          relative_path=self.node.relative_path,
+        )
+      case ASTNode():
+        return Neo4jASTNode(
+          node_id=self.node_id,
+          type=self.node.type,
+          start_line=self.node.start_line,
+          end_line=self.node.end_line,
+          text=self.node.text,
+        )
+      case TextNode():
+        return Neo4jTextNode(node_id=self.node_id, text=self.node.text)
+      case _:
+        raise ValueError("Unknown KnowledgeGraphNode.node type")
+
+
+class KnowledgeGraphEdgeType(enum.StrEnum):
+  """Enum of all knowledge graph edge types"""
+
+  parent_of = "PARENT_OF"
+  has_file = "HAS_FILE"
+  has_ast = "HAS_AST"
+  has_text = "HAS_TEXT"
+  next_chunk = "NEXT_CHUNK"
+
+
+@dataclasses.dataclass
+class KnowledgeGraphEdge:
+  """An edge in the knowledge graph.
+
+  Attributes:
+    source: The source knowledge graph node.
+    target: The target knowledge graph node.
+    type: The knowledge graph edge type.
+  """
+
+  source: KnowledgeGraphNode
+  target: KnowledgeGraphNode
+  type: KnowledgeGraphEdgeType
+
+  def to_neo4j_edge(
+    self,
+  ) -> Union[
+    "Neo4jHasFileEdge",
+    "Neo4jHasASTEdge",
+    "Neo4jParentOfEdge",
+    "Neo4jHasTextEdge",
+    "Neo4jNextChunkEdge",
+  ]:
+    """Convert the KnowledgeGraphEdge into a Neo4j edge format."""
+    match self.type:
+      case KnowledgeGraphEdgeType.has_file:
+        return Neo4jHasFileEdge(
+          source=self.source.to_neo4j_node(), target=self.target.to_neo4j_node()
+        )
+      case KnowledgeGraphEdgeType.has_ast:
+        return Neo4jHasASTEdge(
+          source=self.source.to_neo4j_node(), target=self.target.to_neo4j_node()
+        )
+      case KnowledgeGraphEdgeType.parent_of:
+        return Neo4jParentOfEdge(
+          source=self.source.to_neo4j_node(), target=self.target.to_neo4j_node()
+        )
+      case KnowledgeGraphEdgeType.has_text:
+        return Neo4jHasTextEdge(
+          source=self.source.to_neo4j_node(), target=self.target.to_neo4j_node()
+        )
+      case KnowledgeGraphEdgeType.next_chunk:
+        return Neo4jNextChunkEdge(
+          source=self.source.to_neo4j_node(), target=self.target.to_neo4j_node()
+        )
+      case _:
+        raise ValueError(f"Unknown edge type: {self.type}")
+
+
+###############################################################################
+#                              Neo4j types                                    #
+###############################################################################
+
+
+class Neo4jFileNode(TypedDict):
+  node_id: int
+  basename: str
+  relative_path: str
+
+
+class Neo4jASTNode(TypedDict):
+  node_id: int
+  type: str
+  start_line: int
+  end_line: int
+  text: str
+
+
+class Neo4jTextNode(TypedDict):
+  node_id: int
+  text: str
+
+
+class Neo4jHasFileEdge(TypedDict):
+  source: Neo4jFileNode
+  target: Neo4jFileNode
+
+
+class Neo4jHasASTEdge(TypedDict):
+  source: Neo4jFileNode
+  target: Neo4jASTNode
+
+
+class Neo4jParentOfEdge(TypedDict):
+  source: Neo4jASTNode
+  target: Neo4jASTNode
+
+
+class Neo4jHasTextEdge(TypedDict):
+  source: Neo4jFileNode
+  target: Neo4jTextNode
+
+
+class Neo4jNextChunkEdge(TypedDict):
+  source: Neo4jTextNode
+  target: Neo4jTextNode
---
+++ tests/knowledge_graph/test_types.py
@@ -0,0 +1,220 @@
+from prometheus.knowledge_graph.types import (
+  ASTNode,
+  FileNode,
+  KnowledgeGraphEdge,
+  KnowledgeGraphEdgeType,
+  KnowledgeGraphNode,
+  TextNode,
+)
+
+
+def test_to_neo4j_file_node():
+  basename = "foo"
+  relative_path = "foo/bar/baz.py"
+  node_id = 1
+
+  file_node = FileNode(basename, relative_path)
+  knowldege_graph_node = KnowledgeGraphNode(node_id, file_node)
+  neo4j_file_node = knowldege_graph_node.to_neo4j_node()
+
+  assert isinstance(neo4j_file_node, dict)
+
+  assert "node_id" in neo4j_file_node
+  assert "basename" in neo4j_file_node
+  assert "relative_path" in neo4j_file_node
+
+  assert neo4j_file_node["node_id"] == node_id
+  assert neo4j_file_node["basename"] == basename
+  assert neo4j_file_node["relative_path"] == relative_path
+
+
+def test_to_neo4j_ast_node():
+  type = "method_declaration"
+  start_line = 1
+  end_line = 5
+  text = "print('Hello world')"
+  node_id = 1
+
+  ast_node = ASTNode(type, start_line, end_line, text)
+  knowldege_graph_node = KnowledgeGraphNode(node_id, ast_node)
+  neo4j_ast_node = knowldege_graph_node.to_neo4j_node()
+
+  assert isinstance(neo4j_ast_node, dict)
+
+  assert "node_id" in neo4j_ast_node
+  assert "type" in neo4j_ast_node
+  assert "start_line" in neo4j_ast_node
+  assert "end_line" in neo4j_ast_node
+  assert "text" in neo4j_ast_node
+
+  assert neo4j_ast_node["node_id"] == node_id
+  assert neo4j_ast_node["type"] == type
+  assert neo4j_ast_node["start_line"] == start_line
+  assert neo4j_ast_node["end_line"] == end_line
+  assert neo4j_ast_node["text"] == text
+
+
+def test_to_neo4j_text_node():
+  text = "Hello world"
+  node_id = 1
+
+  text_node = TextNode(text)
+  knowldege_graph_node = KnowledgeGraphNode(node_id, text_node)
+  neo4j_text_node = knowldege_graph_node.to_neo4j_node()
+
+  assert isinstance(neo4j_text_node, dict)
+
+  assert "node_id" in neo4j_text_node
+  assert "text" in neo4j_text_node
+
+  assert neo4j_text_node["node_id"] == node_id
+  assert neo4j_text_node["text"] == text
+
+
+def test_to_neo4j_has_file_edge():
+  source_basename = "source"
+  source_relative_path = "foo/bar/source.py"
+  source_node_id = 1
+  target_basename = "target"
+  target_relative_path = "foo/bar/target.py"
+  target_node_id = 10
+
+  source_file_node = FileNode(source_basename, source_relative_path)
+  source_knowledge_graph_node = KnowledgeGraphNode(source_node_id, source_file_node)
+  target_file_node = FileNode(target_basename, target_relative_path)
+  target_knowledge_graph_node = KnowledgeGraphNode(target_node_id, target_file_node)
+  knowledge_graph_edge = KnowledgeGraphEdge(
+    source_knowledge_graph_node,
+    target_knowledge_graph_node,
+    KnowledgeGraphEdgeType.has_file,
+  )
+  neo4j_has_file_edge = knowledge_graph_edge.to_neo4j_edge()
+
+  assert isinstance(neo4j_has_file_edge, dict)
+
+  assert "source" in neo4j_has_file_edge
+  assert "target" in neo4j_has_file_edge
+
+  assert neo4j_has_file_edge["source"] == source_knowledge_graph_node.to_neo4j_node()
+  assert neo4j_has_file_edge["target"] == target_knowledge_graph_node.to_neo4j_node()
+
+
+def test_to_neo4j_has_ast_edge():
+  source_basename = "source"
+  source_relative_path = "foo/bar/source.py"
+  source_node_id = 1
+  target_type = "return_statement"
+  target_start_line = 7
+  target_end_line = 9
+  target_text = "return True"
+  target_node_id = 10
+
+  source_file_node = FileNode(source_basename, source_relative_path)
+  source_knowledge_graph_node = KnowledgeGraphNode(source_node_id, source_file_node)
+  target_ast_node = ASTNode(
+    target_type, target_start_line, target_end_line, target_text
+  )
+  target_knowledge_graph_node = KnowledgeGraphNode(target_node_id, target_ast_node)
+  knowledge_graph_edge = KnowledgeGraphEdge(
+    source_knowledge_graph_node,
+    target_knowledge_graph_node,
+    KnowledgeGraphEdgeType.has_ast,
+  )
+  neo4j_has_ast_edge = knowledge_graph_edge.to_neo4j_edge()
+
+  assert isinstance(neo4j_has_ast_edge, dict)
+
+  assert "source" in neo4j_has_ast_edge
+  assert "target" in neo4j_has_ast_edge
+
+  assert neo4j_has_ast_edge["source"] == source_knowledge_graph_node.to_neo4j_node()
+  assert neo4j_has_ast_edge["target"] == target_knowledge_graph_node.to_neo4j_node()
+
+
+def test_to_neo4j_parent_of_edge():
+  source_type = "method_declaration"
+  source_start_line = 1
+  source_end_line = 5
+  source_text = "print('Hello world')"
+  source_node_id = 1
+  target_type = "return_statement"
+  target_start_line = 7
+  target_end_line = 9
+  target_text = "return True"
+  target_node_id = 10
+
+  source_ast_node = ASTNode(
+    source_type, source_start_line, source_end_line, source_text
+  )
+  source_knowledge_graph_node = KnowledgeGraphNode(source_node_id, source_ast_node)
+  target_ast_node = ASTNode(
+    target_type, target_start_line, target_end_line, target_text
+  )
+  target_knowledge_graph_node = KnowledgeGraphNode(target_node_id, target_ast_node)
+  knowledge_graph_edge = KnowledgeGraphEdge(
+    source_knowledge_graph_node,
+    target_knowledge_graph_node,
+    KnowledgeGraphEdgeType.parent_of,
+  )
+  neo4j_parent_of_edge = knowledge_graph_edge.to_neo4j_edge()
+
+  assert isinstance(neo4j_parent_of_edge, dict)
+
+  assert "source" in neo4j_parent_of_edge
+  assert "target" in neo4j_parent_of_edge
+
+  assert neo4j_parent_of_edge["source"] == source_knowledge_graph_node.to_neo4j_node()
+  assert neo4j_parent_of_edge["target"] == target_knowledge_graph_node.to_neo4j_node()
+
+
+def test_to_neo4j_has_text_edge():
+  source_basename = "source"
+  source_relative_path = "foo/bar/source.py"
+  source_node_id = 1
+  target_text = "Hello world"
+  target_node_id = 10
+
+  source_file_node = FileNode(source_basename, source_relative_path)
+  source_knowledge_graph_node = KnowledgeGraphNode(source_node_id, source_file_node)
+  target_text_node = TextNode(target_text)
+  target_knowledge_graph_node = KnowledgeGraphNode(target_node_id, target_text_node)
+  knowledge_graph_edge = KnowledgeGraphEdge(
+    source_knowledge_graph_node,
+    target_knowledge_graph_node,
+    KnowledgeGraphEdgeType.has_text,
+  )
+  neo4j_has_text_edge = knowledge_graph_edge.to_neo4j_edge()
+
+  assert isinstance(neo4j_has_text_edge, dict)
+
+  assert "source" in neo4j_has_text_edge
+  assert "target" in neo4j_has_text_edge
+
+  assert neo4j_has_text_edge["source"] == source_knowledge_graph_node.to_neo4j_node()
+  assert neo4j_has_text_edge["target"] == target_knowledge_graph_node.to_neo4j_node()
+
+
+def test_to_neo4j_next_chunk_edge():
+  source_text = "Hello"
+  source_node_id = 1
+  target_text = "world"
+  target_node_id = 10
+
+  source_text_node = TextNode(source_text)
+  source_knowledge_graph_node = KnowledgeGraphNode(source_node_id, source_text_node)
+  target_text_node = TextNode(target_text)
+  target_knowledge_graph_node = KnowledgeGraphNode(target_node_id, target_text_node)
+  knowledge_graph_edge = KnowledgeGraphEdge(
+    source_knowledge_graph_node,
+    target_knowledge_graph_node,
+    KnowledgeGraphEdgeType.has_text,
+  )
+  neo4j_next_chunk_edge = knowledge_graph_edge.to_neo4j_edge()
+
+  assert isinstance(neo4j_next_chunk_edge, dict)
+
+  assert "source" in neo4j_next_chunk_edge
+  assert "target" in neo4j_next_chunk_edge
+
+  assert neo4j_next_chunk_edge["source"] == source_knowledge_graph_node.to_neo4j_node()
+  assert neo4j_next_chunk_edge["target"] == target_knowledge_graph_node.to_neo4j_node()
