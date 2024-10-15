4dc19ba9971a7c446f86098a2ad1f181bab5342b
chenzimin <chen.zimin@hotmail.com>
2024-10-15 16:22:55 +0200

    Add configuration setup

---
+++ prometheus/configuration/config.py
@@ -0,0 +1,12 @@
+import json
+from pathlib import Path
+
+CONFIG_FILE = Path(__file__).parent / "config.json"
+
+
+def load_config():
+  with open(CONFIG_FILE, "r") as f:
+    return json.load(f)
+
+
+config = load_config()
