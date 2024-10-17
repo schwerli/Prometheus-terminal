701dd3433b84c38346271a33461afc22da90c9a3
chenzimin <chen.zimin@hotmail.com>
2024-10-17 21:19:48 +0200

    Add test for fastapi

---
+++ tests/app/api/test_chat.py
@@ -0,0 +1,39 @@
+from unittest.mock import Mock
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+import neo4j
+
+from langchain_community.chat_models.fake import FakeListChatModel
+from prometheus.agents.context_provider_agent import ContextProviderAgent
+from prometheus.app.api import chat
+from prometheus.graph.knowledge_graph import KnowledgeGraph
+
+app = FastAPI()
+app.include_router(chat.router, prefix="/chat", tags=["chat"])
+client = TestClient(app)
+
+class FakeListChatModelWithTools(FakeListChatModel):
+   def bind_tools(self, tools):
+      return self
+
+def test_send_without_kg():
+  response = client.post("/chat/send", json={"query": "hello"})
+  assert response.status_code == 404
+
+
+def test_send():
+    kg_mock = Mock(spec=KnowledgeGraph)
+    kg_mock.get_file_tree.return_value = "mock_file_tree"
+    neo4j_driver_mock = Mock(spec=neo4j.Driver)
+    mock_response = "Mock response"
+    fake_llm = FakeListChatModelWithTools(responses=[mock_response])
+    fake_agent = ContextProviderAgent(llm=fake_llm, kg=kg_mock, neo4j_driver=neo4j_driver_mock)
+
+    app.state.kg = kg_mock
+    app.state.neo4j_driver = neo4j_driver_mock
+    app.state.cp_agent = fake_agent
+
+    response = client.post("/chat/send", json={"query": "hello"})
+    assert response.status_code == 200
+    assert response.json() == mock_response
+
---
+++ tests/app/api/test_repository.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from neo4j import GraphDatabase
+import pytest
+from prometheus.app.api import  repository
+
+from testcontainers.neo4j import Neo4jContainer
+
+from tests.test_utils import test_project_paths
+
+app = FastAPI()
+app.include_router(repository.router, prefix="/repository", tags=["repository"])
+client = TestClient(app)
+
+NEO4J_IMAGE = "neo4j:5.20.0"
+NEO4J_USERNAME = "neo4j"
+NEO4J_PASSWORD = "password"
+
+
+@pytest.fixture(scope="session")
+def setup_neo4j_container():
+  container = Neo4jContainer(
+    image=NEO4J_IMAGE, username=NEO4J_USERNAME, password=NEO4J_PASSWORD
+  ).with_env("NEO4J_PLUGINS", '["apoc"]')
+  with container as neo4j_container:
+    yield neo4j_container
+
+def test_upload_local_repository(setup_neo4j_container):
+  neo4j_container = setup_neo4j_container
+  from prometheus.configuration import config
+  config.config["neo4j"]["uri"] = neo4j_container.get_connection_url()
+  config.config["neo4j"]["username"] = NEO4J_USERNAME
+  config.config["neo4j"]["password"] = NEO4J_PASSWORD
+  config.config["knowledge_graph"]["max_ast_depth"] = 1000
+
+  response = client.post("/repository/local", json={"path": test_project_paths.TEST_PROJECT_PATH.absolute().as_posix()})
+  assert response.status_code == 200
+
+  def _count_num_nodes(tx):
+    result = tx.run("""
+      MATCH (n)
+      RETURN n
+    """)
+    values = [record.values() for record in result]
+    return values
+
+  with GraphDatabase.driver(neo4j_container.get_connection_url(), auth=(NEO4J_USERNAME, NEO4J_PASSWORD)) as driver:
+    with driver.session() as session:
+      read_nodes = session.execute_read(_count_num_nodes)
+      assert len(read_nodes) == 96
