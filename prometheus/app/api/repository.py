bb8b4ac7a532666fc41d9122231863761679b890
chenzimin <chen.zimin@hotmail.com>
2024-10-18 09:48:15 +0200

    Renamn handler to knowledge_graph_handler

--- prometheus/app/api/repository.py
+++ prometheus/app/api/repository.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 
 from prometheus.configuration import config
 from prometheus.graph.knowledge_graph import KnowledgeGraph
-from prometheus.neo4j import handler
+from prometheus.neo4j import knowledge_graph_handler
 
 router = APIRouter()
 
@@ -27,13 +27,13 @@ def upload_local_repository(local_repository: LocalRepository, request: Request)
   kg = KnowledgeGraph(
     Path(local_repository.path), config.config["knowledge_graph"]["max_ast_depth"]
   )
-  neo4j_handler = handler.Handler(
+  knowledge_graph_handler = knowledge_graph_handler.KnowledgeGraphHandler(
     config.config["neo4j"]["uri"],
     config.config["neo4j"]["username"],
     config.config["neo4j"]["password"],
     config.config["neo4j"]["database"],
     config.config["neo4j"]["batch_size"],
   )
-  neo4j_handler.write_knowledge_graph(kg)
-  neo4j_handler.close()
+  knowledge_graph_handler.write_knowledge_graph(kg)
+  knowledge_graph_handler.close()
   request.app.state.kg = kg
--- tests/neo4j/test_knowledge_graph_handler.py
+++ tests/neo4j/test_knowledge_graph_handler.py
@@ -3,7 +3,7 @@ from neo4j import GraphDatabase
 from testcontainers.neo4j import Neo4jContainer
 
 from prometheus.graph.knowledge_graph import KnowledgeGraph
-from prometheus.neo4j.handler import Handler
+from prometheus.neo4j.knowledge_graph_handler import KnowledgeGraphHandler
 from tests.test_utils import test_project_paths
 
 NEO4J_IMAGE = "neo4j:5.20.0"
@@ -19,7 +19,7 @@ def setup_neo4j_container():
   ).with_env("NEO4J_PLUGINS", '["apoc"]')
   with container as neo4j_container:
     uri = neo4j_container.get_connection_url()
-    handler = Handler(uri, NEO4J_USERNAME, NEO4J_PASSWORD, "neo4j", 100)
+    handler = KnowledgeGraphHandler(uri, NEO4J_USERNAME, NEO4J_PASSWORD, "neo4j", 100)
     handler.write_knowledge_graph(kg)
     handler.close()
     yield neo4j_container
