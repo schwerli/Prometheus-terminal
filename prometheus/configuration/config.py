f64c186786c26fd54abce85a45587996ad678170
Zimin <chen.zimin@hotmail.com>
2024-10-22 14:13:07 +0200

    Ruff format

--- prometheus/configuration/config.py
+++ prometheus/configuration/config.py
@@ -12,13 +12,14 @@ def load_config():
   with open(CONFIG_FILE, "r") as f:
     config_data = json.load(f)
 
-  if 'litellm' in config_data:
-    if 'anthropic_api_key' in config_data['litellm']:
-      os.environ['ANTHROPIC_API_KEY'] = config_data['litellm']['anthropic_api_key']
-    elif 'azure_api_key' in config_data['litellm']:
-      os.environ['AZURE_API_KEY'] = config_data['litellm']['azure_api_key']
-    elif 'openai_api_key' in config_data['litellm']:
-      os.environ['OPENAI_API_KEY'] = config_data['litellm']['openai_api_key']
+  if "litellm" in config_data:
+    if "anthropic_api_key" in config_data["litellm"]:
+      os.environ["ANTHROPIC_API_KEY"] = config_data["litellm"]["anthropic_api_key"]
+    elif "azure_api_key" in config_data["litellm"]:
+      os.environ["AZURE_API_KEY"] = config_data["litellm"]["azure_api_key"]
+    elif "openai_api_key" in config_data["litellm"]:
+      os.environ["OPENAI_API_KEY"] = config_data["litellm"]["openai_api_key"]
   return config_data
+
 
 config = load_config()
