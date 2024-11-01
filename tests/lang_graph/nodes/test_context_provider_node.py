8cf289685789e0f9f870c4d2bbfe629d16700383
chenzimin <chen.zimin@hotmail.com>
2024-11-01 22:17:49 +0100

    Add noqa F401 for fixture

--- tests/lang_graph/nodes/test_context_provider_node.py
+++ tests/lang_graph/nodes/test_context_provider_node.py
@@ -2,7 +2,8 @@ from langchain_core.messages import AIMessage, ToolMessage
 
 from prometheus.lang_graph.nodes.context_provider_node import ContextProviderNode
 from prometheus.lang_graph.subgraphs.context_provider_state import ContextProviderState
-from tests.test_utils.util import FakeListChatWithToolsModel  # noqa: F401
+from tests.test_utils.fixtures import neo4j_container_with_kg_fixture  # noqa: F401
+from tests.test_utils.util import FakeListChatWithToolsModel
 
 
 def test_context_provider_node(neo4j_container_with_kg_fixture):  # noqa: F811
