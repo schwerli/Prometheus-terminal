1ceb6558e764b89cfbccc302da82e7d2d4af17a9
chenzimin <chen.zimin@hotmail.com>
2024-11-09 23:59:11 +0100

    Add debug message in IssueAnswerAndFixSuccessRouter

--- prometheus/lang_graph/routers/issue_answer_and_fix_success_router.py
+++ prometheus/lang_graph/routers/issue_answer_and_fix_success_router.py
@@ -1,8 +1,14 @@
+import logging
+
 from prometheus.lang_graph.subgraphs.issue_answer_and_fix_state import IssueAnswerAndFixState
 
 
 class IssueAnswerAndFixSuccessRouter:
+  def __init__(self):
+    self._logger = logging.getLogger("prometheus.lang_graph.routers.issue_answer_and_fix_success_router")
+
   def __call__(self, state: IssueAnswerAndFixState):
+    self._logger.debug(f"Running IssueAnswerAndFixSuccessRouter for state:\n{state}")
     if not state["run_build"] and not state["run_test"]:
       return True
 
