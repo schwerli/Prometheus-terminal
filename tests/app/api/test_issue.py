ffebe79a1af023ddc54490cf1f40947710ba6a90
chenzimin <chen.zimin@hotmail.com>
2024-11-09 23:25:05 +0100

    Fix the failing test

--- tests/app/api/test_issue.py
+++ tests/app/api/test_issue.py
@@ -20,15 +20,17 @@ def mock_service_coordinator():
 
 def test_answer_and_fix_issue(mock_service_coordinator):
   mock_service_coordinator.exists_knowledge_graph.return_value = False
-  response = client.post("/issue/answer_and_fix/", json={"title": "title", "body": "body"})
+  response = client.post("/issue/answer_and_fix/", json={"number": 1, "title": "title", "body": "body"})
   assert response.status_code == 404
 
 
 def test_send(mock_service_coordinator):
   mock_service_coordinator.exists_knowledge_graph.return_value = True
-  mock_response = "mock response"
+  fake_response = "fake response"
+  fake_branch_name = "fake_remote_branch_name"
+  mock_response = (fake_response, fake_branch_name)
   mock_service_coordinator.answer_and_fix_issue.return_value = mock_response
-  response = client.post("/issue/answer_and_fix/", json={"title": "title", "body": "body"})
+  response = client.post("/issue/answer_and_fix/", json={"number": 1, "title": "title", "body": "body"})
 
   assert response.status_code == 200
-  assert response.json() == mock_response
+  assert response.json() == {"issue_response": fake_response, "remote_branch_name": fake_branch_name}
