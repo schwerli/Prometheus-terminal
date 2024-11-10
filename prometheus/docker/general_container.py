25d6fe46f954f1c245057edfc3d90900454f77c0
Zimin <ziche94@gmail.com>
2024-11-10 12:37:40 +0100

    Include docker in general container

--- prometheus/docker/general_container.py
+++ prometheus/docker/general_container.py
@@ -44,7 +44,18 @@ RUN apt-get update && apt-get install -y \
     vim \
     nano \
     zip \
-    unzip
+    unzip \
+    ca-certificates \
+    gnupg \
+    lsb-release
+
+RUN mkdir -p /etc/apt/keyrings \
+    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg \
+    && echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+RUN apt-get update && apt-get install -y docker-ce-cli
 
 RUN apt-get clean
 RUN rm -rf /var/lib/apt/lists/*
