a8e3b25848a9ef37d86f1074c22725a2331312c3
Zimin <ziche94@gmail.com>
2024-11-12 21:50:51 +0100

    Create working dir first

--- prometheus/app/main.py
+++ prometheus/app/main.py
@@ -19,6 +19,7 @@ console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 
 # File handler
+os.makedirs(settings.WORKING_DIRECTORY, exist_ok=True)
 file_handler = logging.FileHandler(os.path.join(settings.WORKING_DIRECTORY, "prometheus.log"))
 file_handler.setFormatter(formatter)
 
