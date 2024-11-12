106bd27b68a61f6ef7232d53b2b289261a212dc6
Zimin <ziche94@gmail.com>
2024-11-12 22:35:22 +0100

    Fix test_main

--- prometheus/app/main.py
+++ prometheus/app/main.py
@@ -36,7 +36,7 @@ logger.propagate = False
 async def lifespan(app: FastAPI):
   app.state.service_coordinator = dependencies.initialize_services()
   yield
-  app.state.service_coordiantor.clear()
+  app.state.service_coordinator.clear()
   app.state.service_coordinator.close()
 
 
