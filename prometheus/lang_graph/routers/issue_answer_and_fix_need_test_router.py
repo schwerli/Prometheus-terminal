ebe1d51aa85c51c4ebba6ec2a02ad0c732ede003
chenzimin <chen.zimin@hotmail.com>
2024-11-14 13:43:48 +0100

    Fix ruff format

--- prometheus/lang_graph/routers/issue_answer_and_fix_need_test_router.py
+++ prometheus/lang_graph/routers/issue_answer_and_fix_need_test_router.py
@@ -8,23 +8,23 @@ from prometheus.lang_graph.subgraphs.issue_answer_and_fix_state import IssueAnsw
 
 
 class IssueAnswerAndFixNeedTestRouter:
-    """Router to determine if testing is required in the issue answer and fix workflow.
+  """Router to determine if testing is required in the issue answer and fix workflow.
 
-    This router checks the state to determine whether testing should be run
-    as part of the issue resolution process.
+  This router checks the state to determine whether testing should be run
+  as part of the issue resolution process.
 
-    Attributes:
-        None
+  Attributes:
+      None
+  """
+
+  def __call__(self, state: IssueAnswerAndFixState):
+    """Determine if testing should be run based on the current workflow state.
+
+    Args:
+        state (IssueAnswerAndFixState): The current state of the issue
+            answer and fix workflow.
+
+    Returns:
+        bool: True if tests should be run, False otherwise.
     """
-
-    def __call__(self, state: IssueAnswerAndFixState):
-        """Determine if testing should be run based on the current workflow state.
-
-        Args:
-            state (IssueAnswerAndFixState): The current state of the issue 
-                answer and fix workflow.
-
-        Returns:
-            bool: True if tests should be run, False otherwise.
-        """
-        return state["run_test"]
+    return state["run_test"]
