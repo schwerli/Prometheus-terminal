8430b3b0f9f2a200e95152ce9860be612f5cb825
chenzimin <chen.zimin@hotmail.com>
2024-11-18 11:18:23 +0100

    Use each chat class instead of chatlitellm

--- prometheus/app/services/llm_service.py
+++ prometheus/app/services/llm_service.py
@@ -1,10 +1,18 @@
 import logging
 
-from langchain_community.chat_models import ChatLiteLLM
+from langchain_anthropic import ChatAnthropic
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
 
 
 class LLMService:
   def __init__(self, model_name: str):
     self._logger = logging.getLogger("prometheus.app.services.llm_service")
-    self.model = ChatLiteLLM(model=model_name)
+
+    if "claude" in model_name:
+      self.model = ChatAnthropic(model=model_name, max_tokens=8192)
+    elif "gpt" in model_name:
+      self.model = ChatOpenAI(model=model_name, max_tokens=None)
+    elif "gemini" in model_name:
+      self.model = ChatGoogleGenerativeAI(model=model_name, max_tokens=None)
     self._logger.info(f"LLM model: {self.model.model}")
--- prometheus/configuration/config.py
+++ prometheus/configuration/config.py
@@ -11,6 +11,6 @@ settings = Dynaconf(
 if "LITELLM_ANTHROPIC_API_KEY" in settings:
   os.environ["ANTHROPIC_API_KEY"] = settings.LITELLM_ANTHROPIC_API_KEY
 if "LITELLM_GEMINI_API_KEY" in settings:
-  os.environ["GEMINI_API_KEY"] = settings.LITELLM_GEMINI_API_KEY
+  os.environ["GOOGLE_API_KEY"] = settings.LITELLM_GEMINI_API_KEY
 if "LITELLM_OPENAI_API_KEY" in settings:
   os.environ["OPENAI_API_KEY"] = settings.LITELLM_OPENAI_API_KEY
