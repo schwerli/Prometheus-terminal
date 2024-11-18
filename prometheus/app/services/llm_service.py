3c90d4447d899334b23c641157811fee7137fc9f
chenzimin <chen.zimin@hotmail.com>
2024-11-18 21:15:47 +0100

    Remove logging in LLM service as it is already logged else

--- prometheus/app/services/llm_service.py
+++ prometheus/app/services/llm_service.py
@@ -7,12 +7,9 @@ from langchain_openai import ChatOpenAI
 
 class LLMService:
   def __init__(self, model_name: str):
-    self._logger = logging.getLogger("prometheus.app.services.llm_service")
-
     if "claude" in model_name:
       self.model = ChatAnthropic(model=model_name, max_tokens=8192, max_retries=3)
     elif "gpt" in model_name:
       self.model = ChatOpenAI(model=model_name, max_tokens=None, max_retries=3)
     elif "gemini" in model_name:
       self.model = ChatGoogleGenerativeAI(model=model_name, max_tokens=None, max_retries=3)
-    self._logger.info(f"LLM model: {model_name}")
