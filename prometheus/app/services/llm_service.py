e7d584857124fd91a06bf99320711bd8f5a5bbaf
chenzimin <chen.zimin@hotmail.com>
2024-11-18 21:04:33 +0100

    Add max retry

--- prometheus/app/services/llm_service.py
+++ prometheus/app/services/llm_service.py
@@ -10,9 +10,9 @@ class LLMService:
     self._logger = logging.getLogger("prometheus.app.services.llm_service")
 
     if "claude" in model_name:
-      self.model = ChatAnthropic(model=model_name, max_tokens=8192)
+      self.model = ChatAnthropic(model=model_name, max_tokens=8192, max_retries=3)
     elif "gpt" in model_name:
-      self.model = ChatOpenAI(model=model_name, max_tokens=None)
+      self.model = ChatOpenAI(model=model_name, max_tokens=None, max_retries=3)
     elif "gemini" in model_name:
-      self.model = ChatGoogleGenerativeAI(model=model_name, max_tokens=None)
+      self.model = ChatGoogleGenerativeAI(model=model_name, max_tokens=None, max_retries=3)
     self._logger.info(f"LLM model: {model_name}")
