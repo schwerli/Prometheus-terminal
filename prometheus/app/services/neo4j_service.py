279cfde69b81d5620694cf853f26ad59a9072219
Yue Pan <79363355+dcloud347@users.noreply.github.com>
2025-06-25 15:29:43 +0800

    Increase Neo4j transaction timeout and connection timeout settings for improved performance

--- prometheus/app/services/neo4j_service.py
+++ prometheus/app/services/neo4j_service.py
@@ -8,8 +8,8 @@ class Neo4jService:
         self.neo4j_driver = GraphDatabase.driver(
             neo4j_uri,
             auth=(neo4j_username, neo4j_password),
-            connection_timeout=300,
-            max_transaction_retry_time=300,
+            connection_timeout=600,
+            max_transaction_retry_time=600,
             keep_alive=True,
         )
 
