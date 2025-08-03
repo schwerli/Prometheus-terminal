1256c33fb9201a70e442b5bdae50685cb8509d2d
Yue Pan <79363355+dcloud347@users.noreply.github.com>
2025-08-04 00:43:14 +0800

    Add Settings class for configuration management using Pydantic

--- prometheus/configuration/config.py
+++ prometheus/configuration/config.py
@@ -1,3 +1,52 @@
-from dynaconf import Dynaconf
+from typing import Literal, List
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-settings = Dynaconf(envvar_prefix="PROMETHEUS", load_dotenv=True)
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env',
+                                      env_file_encoding='utf-8',
+                                      env_prefix='PROMETHEUS_'
+                                      )
+    # General settings
+    version: str = "1.1"
+    BASE_URL: str = f"/api/v{version}"
+    ENVIRONMENT: Literal["local", "production"]
+    BACKEND_CORS_ORIGINS: List[str]
+    PROJECT_NAME: str = "Prometheus"
+
+    # Logging
+    LOGGING_LEVEL: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+
+    # Neo4j
+    NEO4J_URI: str
+    NEO4J_USERNAME: str
+    NEO4J_PASSWORD: str
+    NEO4J_BATCH_SIZE: int
+
+    # Knowledge Graph
+    WORKING_DIRECTORY: str
+    KNOWLEDGE_GRAPH_MAX_AST_DEPTH: int
+    KNOWLEDGE_GRAPH_CHUNK_SIZE: int
+    KNOWLEDGE_GRAPH_CHUNK_OVERLAP: int
+    MAX_TOKEN_PER_NEO4J_RESULT: int
+
+    # LLM models
+    ADVANCED_MODEL: str
+    BASE_MODEL: str
+
+    # API Keys
+    ANTHROPIC_API_KEY: str
+    GEMINI_API_KEY: str
+    OPENAI_FORMAT_BASE_URL: str
+    OPENAI_API_KEY: str
+
+    # Model parameters
+    MAX_INPUT_TOKENS: int
+    TEMPERATURE: float
+    MAX_OUTPUT_TOKENS: int
+
+    # Database
+    DATABASE_URL: str
+
+
+settings = Settings()
