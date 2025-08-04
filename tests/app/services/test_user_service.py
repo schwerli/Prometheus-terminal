1726289674174af92a8301ea4a0627f6d3a27aa2
Yue Pan <79363355+dcloud347@users.noreply.github.com>
2025-08-04 22:54:12 +0800

    Refactor superuser creation test for improved readability

--- tests/app/services/test_user_service.py
+++ tests/app/services/test_user_service.py
@@ -2,7 +2,6 @@ import pytest
 
 from prometheus.app.services.database_service import DatabaseService
 from prometheus.app.services.user_service import UserService
-from tests.test_utils.fixtures import postgres_container_fixture
 
 
 @pytest.fixture
@@ -15,11 +14,7 @@ def mock_database_service(postgres_container_fixture):
 def test_create_superuser(mock_database_service):
     # Exercise
     service = UserService(mock_database_service)
-    service.create_superuser(
-        "testuser",
-        "test@gmail.com",
-        "password123",
-        github_token="gh_token"
-    )
+    service.create_superuser("testuser", "test@gmail.com", "password123", github_token="gh_token")
+
 
 # TODO test login
