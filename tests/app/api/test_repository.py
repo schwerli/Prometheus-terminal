6179b2793380291847f1740122748dfaf5a78c75
Yue Pan <79363355+dcloud347@users.noreply.github.com>
2025-08-04 20:55:38 +0800

    Remove unused test functions for local repository uploads

--- tests/app/api/test_repository.py
+++ tests/app/api/test_repository.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 from prometheus.app.api import repository
-from tests.test_utils import test_project_paths
 
 app = FastAPI()
 app.include_router(repository.router, prefix="/repository", tags=["repository"])
@@ -17,23 +16,6 @@ def mock_service_coordinator():
     service_coordinator = mock.MagicMock()
     app.state.service_coordinator = service_coordinator
     yield service_coordinator
-
-
-def test_upload_local_repository(mock_service_coordinator):
-    mock_service_coordinator.upload_local_repository.return_value = None
-    response = client.get(
-        "/repository/local",
-        params={"local_repository": test_project_paths.TEST_PROJECT_PATH.absolute().as_posix()},
-    )
-    assert response.status_code == 200
-
-
-def test_upload_fake_local_repository():
-    response = client.get(
-        "/repository/local/",
-        params={"local_repository": "/foo/bar/"},
-    )
-    assert response.status_code == 404
 
 
 def test_delete(mock_service_coordinator):
