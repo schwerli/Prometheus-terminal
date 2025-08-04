439a9e87440623c3fab6ec9b89e7c27043d793c8
Yue Pan <79363355+dcloud347@users.noreply.github.com>
2025-08-05 00:09:04 +0800

    Add unit tests for authentication login endpoint

---
+++ tests/app/api/test_auth.py
@@ -0,0 +1,38 @@
+from unittest import mock
+
+import pytest
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from prometheus.app.api import auth
+from prometheus.app.exception_handler import register_exception_handlers
+
+app = FastAPI()
+register_exception_handlers(app)
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
+client = TestClient(app)
+
+
+@pytest.fixture
+def mock_service():
+    service = mock.MagicMock()
+    app.state.service = service
+    yield service
+
+
+def test_login(mock_service):
+    mock_service["user_service"].login.return_value = "your_access_token"
+    response = client.post(
+        "/auth/login",
+        json={
+            "username": "testuser",
+            "email": "test@gmail.com",
+            "password": "passwordpassword",
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "code": 200,
+        "message": "success",
+        "data": {"access_token": "your_access_token"},
+    }
