b8214ae17e85fa19b6ada832e7fcd285a4445e9e
Yue Pan <79363355+dcloud347@users.noreply.github.com>
2025-08-04 16:13:27 +0800

    Refactor JWTUtils to use double quotes for string literals and improve code formatting

--- prometheus/utils/jwt_utils.py
+++ prometheus/utils/jwt_utils.py
@@ -1,14 +1,15 @@
 import datetime
-import jwt
-from prometheus.configuration.config import settings
 
+import jwt
+
+from prometheus.configuration.config import settings
 from prometheus.exceptions.jwt_exception import JWTException
 
 
 class JWTUtils:
     """JWT Utility for token generation and validation"""
 
-    def __init__(self, algorithm='HS256'):
+    def __init__(self, algorithm="HS256"):
         """Initialize JWT utils with configuration"""
         self.secret_key = settings.JWT_SECRET_KEY
         self.algorithm = algorithm
@@ -17,7 +18,7 @@ class JWTUtils:
     def generate_token(self, payload):
         """Generate JWT token"""
         payload_copy = payload.copy()
-        payload_copy['exp'] = datetime.datetime.now() + datetime.timedelta(days=self.expire_time)
+        payload_copy["exp"] = datetime.datetime.now() + datetime.timedelta(days=self.expire_time)
         token = jwt.encode(payload_copy, self.secret_key, algorithm=self.algorithm)
         return token
 
