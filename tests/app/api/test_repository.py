73e140a6be939407a39ba35aa5feb07f96587560
Yue Pan <79363355+dcloud347@users.noreply.github.com>
2025-08-08 12:24:46 +0800

    Add repository_id to issue answering tests and update repository upload endpoints

--- tests/app/api/test_repository.py
+++ tests/app/api/test_repository.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 from prometheus.app.api import repository
+from prometheus.app.entity.repository import Repository
 from prometheus.app.exception_handler import register_exception_handlers
 
 app = FastAPI()
@@ -22,9 +23,9 @@ def mock_service():
 
 def test_upload_repository(mock_service):
     mock_service["repository_service"].clone_github_repo.return_value = "/mock/path"
-    response = client.get(
-        "/repository/github/",
-        params={
+    response = client.post(
+        "/repository/upload",
+        json={
             "github_token": "mock_token",
             "https_url": "https://github.com/Pantheon-temple/Prometheus",
         },
@@ -34,9 +35,9 @@ def test_upload_repository(mock_service):
 
 def test_upload_repository_at_commit(mock_service):
     mock_service["repository_service"].clone_github_repo.return_value = "/mock/path"
-    response = client.get(
-        "/repository/github_commit/",
-        params={
+    response = client.post(
+        "/repository/upload/",
+        json={
             "github_token": "mock_token",
             "https_url": "https://github.com/Pantheon-temple/Prometheus",
             "commit_id": "0c554293648a8705769fa53ec896ae24da75f4fc",
@@ -46,7 +47,24 @@ def test_upload_repository_at_commit(mock_service):
 
 
 def test_delete(mock_service):
-    mock_service["knowledge_graph_service"].exists.return_value = True
-    mock_service["knowledge_graph_service"].clear.return_value = None
-    response = client.get("repository/delete")
+    mock_service["repository_service"].get_repository_by_id.return_value = Repository(
+        id=1,
+        url="https://github.com/fake/repo.git",
+        commit_id=None,
+        playground_path="/path/to/playground",
+        kg_root_node_id=0,
+        user_id=None,
+        kg_max_ast_depth=100,
+        kg_chunk_size=1000,
+        kg_chunk_overlap=100,
+    )
+    mock_service["knowledge_graph_service"].clear_kg.return_value = None
+    mock_service["repository_service"].clean_repository.return_value = None
+    mock_service["repository_service"].delete_repository.return_value = None
+    response = client.delete(
+        "repository/delete",
+        params={
+            "repository_id": 1,
+        },
+    )
     assert response.status_code == 200
